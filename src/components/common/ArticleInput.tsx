a38bcb053117a87a0691c24e52d240fa2be3e739
boong <taurusboong2@gmail.com>
2022-07-13 02:36:34 +0900

    forwardRef type issue 2

--- src/components/common/ArticleInput.tsx
+++ src/components/common/ArticleInput.tsx
@@ -1,25 +1,27 @@
-import React, { ForwardRefExoticComponent, LegacyRef, forwardRef } from 'react';
+import React, { LegacyRef, forwardRef, ForwardRefExoticComponent } from 'react';
 
 type Props = {
-  ref: LegacyRef<HTMLInputElement | HTMLTextAreaElement> | undefined;
   input: boolean;
   placeholder: string;
+  ref: any;
 };
 
-const ArticleInput: ForwardRefExoticComponent<Props> = forwardRef((props, ref) => {
-  return (
-    <>
-      {props.input ? (
-        <fieldset className="form-group">
-          <input type="text" className="form-control form-control-lg" placeholder={props.placeholder} ref={ref} />
-        </fieldset>
-      ) : (
-        <fieldset className="form-group">
-          <textarea className="form-control" rows={8} placeholder={props.placeholder} ref={ref} />
-        </fieldset>
-      )}
-    </>
-  );
-});
+const ArticleInput: ForwardRefExoticComponent<Props> = forwardRef(
+  (props, ref: LegacyRef<HTMLInputElement | HTMLTextAreaElement | undefined>) => {
+    return (
+      <>
+        {props.input ? (
+          <fieldset className="form-group">
+            <input type="text" className="form-control form-control-lg" placeholder={props.placeholder} ref={ref} />
+          </fieldset>
+        ) : (
+          <fieldset className="form-group">
+            <textarea className="form-control" rows={8} placeholder={props.placeholder} ref={ref} />
+          </fieldset>
+        )}
+      </>
+    );
+  }
+);
 
 export default ArticleInput;
