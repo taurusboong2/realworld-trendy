2b323635ef2f314b64d8b02e4a6c8bd238d4e274
boong <taurusboong2@gmail.com>
2022-07-16 11:46:06 +0900

    add profileBox component

---
+++ src/components/Article/ProfileBox.tsx
@@ -0,0 +1,55 @@
+import React, { FC, ReactEventHandler } from 'react';
+import { useFetchCurrentUser } from '../../hooks/auth.hook';
+import { ArticleType } from '../../types/article';
+import MyLink from '../common/MyLink';
+
+type Props = {
+  articleData: ArticleType;
+  deleteHandler: ReactEventHandler<HTMLButtonElement>;
+};
+
+const ProfileBox: FC<Props> = ({ articleData, deleteHandler }) => {
+  const { data: currentUser } = useFetchCurrentUser();
+  return (
+    <>
+      <div className="article-actions">
+        <div className="article-meta">
+          <a href="profile.html">
+            <img src={articleData!.author.image} />
+          </a>
+          <div className="info">
+            <a href="" className="author">
+              {articleData!.author.username}
+            </a>
+            <span className="date"> {new Date(articleData.createdAt).toDateString()}</span>
+          </div>
+          {currentUser !== undefined ? (
+            <>
+              <MyLink href="/editor/[pid]" className="btn btn-outline-secondary btn-sm">
+                <i className="ion-edit" /> Edit Article
+              </MyLink>
+              &nbsp;&nbsp;
+              <button className="btn btn-outline-danger btn-sm" onClick={deleteHandler}>
+                <i className="ion-trash-a" /> Delete articleData
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="btn btn-sm btn-outline-secondary">
+                <i className="ion-plus-round" />
+                &nbsp; Follow {articleData.author.username}
+              </button>
+              &nbsp;
+              <button className="btn btn-sm btn-outline-primary">
+                <i className="ion-heart" />
+                &nbsp; Favorite Post <span className="counter">(29)</span>
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileBox;
--- src/pages/articleDetail.tsx
+++ src/pages/articleDetail.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import LoadingSpinner from '../components/common/LoadingSpinner';
-import { useFetchArticle } from '../hooks/article.hook';
+import { useDeleteArticle, useFetchArticle } from '../hooks/article.hook';
 import Banner from '../components/Article/Banner';
 import Container from '../components/Article/Container';
 import { ArticleType } from '../types/article';
+import ProfileBox from '../components/Article/ProfileBox';
 
 const ArticleDetail = () => {
   const { slug } = useParams();
   const { data, isLoading } = useFetchArticle(slug as string);
+  const { mutate: deleteArticle } = useDeleteArticle();
+
+  const submitDeleteArticle = async () => {
+    const result = confirm('정말로 게시글을 삭제하시겠습니까?');
+    if (!result) return;
+    await deleteArticle(slug as string);
+  };
 
   const articleData = data?.data.article;
   console.log(`data:`, articleData);
@@ -21,6 +29,11 @@ const ArticleDetail = () => {
 
         <div className="container page">
           <Container articleData={articleData as ArticleType} />
+
+          <hr />
+
+          <ProfileBox articleData={articleData as ArticleType} deleteHandler={submitDeleteArticle} />
+
           <div className="row">
             <div className="col-xs-12 col-md-8 offset-md-2">커멘트리스트 자리</div>
           </div>
