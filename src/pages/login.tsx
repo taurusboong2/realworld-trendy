e94dfaff7fca58105013fec44407ac727217a7c2
boong <taurusboong2@gmail.com>
2022-07-22 17:24:38 +0900

    implement basic validation login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,31 +1,18 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import MyLink from '../components/common/MyLink';
 import { useLogin } from '../hooks/auth.hook';
 import LoadingSpinner from '../components/common/LoadingSpinner';
 import { useForm } from 'react-hook-form';
+import { LoginData } from '../types/auth';
 
 const Login = () => {
-  const { register, handleSubmit, watch } = useForm();
-
-  const emailInputRef = useRef<HTMLInputElement>(null);
-  const passWordInputRef = useRef<HTMLInputElement>(null);
+  const { register, handleSubmit } = useForm<LoginData>();
 
   const { mutateAsync: login, status } = useLogin();
 
-  // const loginSubmit = async () => {
-  //   const loginData = {
-  //     user: {
-  //       email: emailInputRef.current?.value as string,
-  //       password: passWordInputRef.current?.value as string,
-  //     },
-  //   };
-  //   await login(loginData);
-  // };
-
-  const onSubmit = data => console.log(data);
-
-  console.log(`email watch`, watch('emailRef'));
-  console.log(`password watch`, watch('passwordRef'));
+  const loginSubmit = async (register: LoginData) => {
+    await login(register);
+  };
 
   if (status === 'loading') return <LoadingSpinner />;
   return (
@@ -39,36 +26,31 @@ const Login = () => {
                 <MyLink href="/signUp">Need an account?</MyLink>
               </p>
 
-              <form onSubmit={handleSubmit(onSubmit)}>
+              <form onSubmit={handleSubmit(loginSubmit)}>
                 <fieldset>
                   <fieldset className="form-group">
                     <input
-                      {...(register('emailRef'),
-                      {
-                        required: true,
-                      })}
+                      {...register('user.email', { required: true })}
                       className="form-control form-control-lg"
                       type="email"
                       placeholder="Email"
-                      ref={emailInputRef}
                     />
                   </fieldset>
 
                   <fieldset className="form-group">
                     <input
-                      {...(register('passwordRef'),
-                      {
-                        required: true,
-                      })}
+                      {...register('user.password', { required: true })}
                       className="form-control form-control-lg"
                       type="password"
                       autoComplete="on"
                       placeholder="Password"
-                      ref={passWordInputRef}
                     />
                   </fieldset>
 
-                  <button className="btn btn-lg btn-primary pull-xs-right" type="submit">
+                  <button
+                    className="btn btn-lg btn-primary pull-xs-right"
+                    type="button"
+                    onClick={handleSubmit(loginSubmit)}>
                     Sign in
                   </button>
                 </fieldset>
