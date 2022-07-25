9bb1696e2f43b0edd8a751b4600bb159289b83a7
ashnamuh <ashnamuh@gmail.com>
2022-07-25 20:26:23 +0900

    index.html 파일 분리

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const config = {
         exclude: /node_modules/,
       },
       {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         use: [
           'style-loader',
           {
@@ -30,7 +30,7 @@ const config = {
             options: {
               importLoaders: 1,
               modules: {
-                localIdentName: '[local]--[hash:base64:5]',
+                localIdentName: '[local]',
               },
               sourceMap: true,
             },
@@ -51,16 +51,8 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      templateContent: ({ htmlWebpackPlugin }) =>
-        '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
-        htmlWebpackPlugin.options.title +
-        '</title>' +
-        '<link rel="stylesheet" href="//demo.productionready.io/main.css" />' +
-        '</head><body><div id="app"></div></body></html>',
+      template: './src/index.html',
       filename: 'index.html',
-      options: {
-        title: 'TauBoong | Realworld',
-      },
     }),
     // new HtmlWebpackPlugin({
     //   template: './public/index.html',
