667e9537b8584a81a32bb9205b11aac8c0c2fb51
boong <taurusboong2@gmail.com>
2022-07-26 01:14:55 +0900

    add constants file & add error message

---
+++ src/commons/errorStyledComponents.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ErrorMessage = styled.small.attrs({
+  role: 'alert',
+})`
+  color: #f15e5e;
+  font-weight: bold;
+`;
---
+++ src/constants/errorMessage.ts
@@ -0,0 +1,7 @@
+export const WRONG_email = '*이메일 형식이 아닙니다.';
+
+export const REQUIRED_message = '*필수 항목입니다.';
+
+export const MIN_length_2 = '*최소 2글자 이상이어야 합니다.';
+
+export const MIN_length_4 = '*최소 4글자 이상이어야 합니다.';
