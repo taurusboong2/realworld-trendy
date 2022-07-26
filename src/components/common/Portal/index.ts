9165c096031b56b50006b0d8cd5c9e6e8d0e31db
boong <taurusboong2@gmail.com>
2022-07-26 16:27:23 +0900

    add Portal component

---
+++ src/components/common/Portal/index.ts
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+type Props = {
+  children: ReactNode;
+};
+
+const Portal = (props: Props) => {
+  const { children } = props;
+
+  const container = document.querySelector(`#portal`) as Element;
+
+  return createPortal(children, container);
+};
+
+export default Portal;
