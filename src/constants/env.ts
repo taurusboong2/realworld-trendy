c064c86437e19c895c5b75330daa63667145492c
ashnamuh <ashnamuh@gmail.com>
2022-08-01 20:50:30 +0900

    센트리 DSN 환경변수화

---
+++ src/constants/env.ts
@@ -0,0 +1,1 @@
+export const SentryDSN = 'https://cff455d7b78644e887d6e7e0e79c195b@o252524.ingest.sentry.io/6616592';
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { SentryDSN } from '@/constants/env';
 
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
 
 Sentry.init({
-  dsn: 'https://cff455d7b78644e887d6e7e0e79c195b@o252524.ingest.sentry.io/6616592',
+  dsn: SentryDSN,
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
 });
