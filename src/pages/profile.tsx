1d3aa1e3cb814f154e14f3d024ab36ca1587e34a
ashnamuh <ashnamuh@gmail.com>
2022-08-01 21:00:39 +0900

    페이보릿 아티클 삭제

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -21,14 +21,7 @@ const Profile = () => {
               <div className="articles-toggle">
                 <ul className="nav nav-pills outline-active">
                   <li className="nav-item">
-                    <a className="nav-link active" href="">
-                      My Articles
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="">
-                      Favorited Articles
-                    </a>
+                    <span className="nav-link active">My Articles</span>
                   </li>
                 </ul>
               </div>
