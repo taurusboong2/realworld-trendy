e71764601cc420b8d269fb2aeaef63d9e8104755
boong <taurusboong2@gmail.com>
2022-08-03 00:17:42 +0900

    Rename : index.scss to toast.scss

--- src/components/common/Toast/index.tsx
+++ src/components/common/Toast/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Toast from './Toast';
 import { Props } from './types';
-import './index.scss';
+import './toast.scss';
 
 export const CONTAINER_CLASSNAME = 'boong_ui-toastContainer';
 
