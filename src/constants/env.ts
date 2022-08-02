afb1b3637100b2a9065aba4515e8fbbec610782c
boong <taurusboong2@gmail.com>
2022-08-02 18:41:54 +0900

    separate sentry dsn as variable

---
+++ src/constants/env.ts
@@ -0,0 +1,1 @@
+export const SentryDSN = 'https://6d4cb3dace88448880bd68b93c90e5bb@o1343553.ingest.sentry.io/6618387';
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
+import { SentryDSN } from '@/constants/env';
 
 Sentry.init({
-  dsn: 'https://6d4cb3dace88448880bd68b93c90e5bb@o1343553.ingest.sentry.io/6618387',
+  dsn: SentryDSN,
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
 });
