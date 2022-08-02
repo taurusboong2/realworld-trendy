e40b0fcb55415a6fae09c1ac9693c9eec84bab9b
boong <taurusboong2@gmail.com>
2022-08-02 17:57:51 +0900

    chore : add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://6d4cb3dace88448880bd68b93c90e5bb@o1343553.ingest.sentry.io/6618387',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const mountNode = document.getElementById('app');
 ReactDOM.render(<App />, mountNode);
