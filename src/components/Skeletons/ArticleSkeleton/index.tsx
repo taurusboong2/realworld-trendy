d6660b0b7a0b0405d074f14312b99ce13ff1160d
boong <taurusboong2@gmail.com>
2022-08-04 23:04:38 +0900

    Rename : fix to scss module

--- src/components/Skeletons/ArticleSkeleton/index.tsx
+++ src/components/Skeletons/ArticleSkeleton/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import './articleSkeleton.scss';
+import './articleSkeleton.module.scss';
 
 const ArticleSkeleton: FC = () => {
   return (
--- src/components/Skeletons/UserInfoSkeleton/index.tsx
+++ src/components/Skeletons/UserInfoSkeleton/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import './UserInfoSkeleton.scss';
+import './UserInfoSkeleton.module.scss';
 
 const UserInfoSkeleton: FC = () => {
   return (
