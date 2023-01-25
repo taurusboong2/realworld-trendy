4aa6ca640f9bada7020e01787151f1a0b3664a0b
boong <taurusboong2@gmail.com>
2023-01-25 18:46:53 +0900

    refactor: import system refactoring
    refactoring

---
+++ src/components/common/index.ts
@@ -0,0 +1,8 @@
+export { default as Box } from './Box';
+export { default as Feed } from './Feed';
+export { default as Footer } from './Footer';
+export { default as Layout } from './Layout';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as MyLink } from './MyLink';
+export { default as Navbar } from './Navbar';
+export { default as NotFound } from './NotFound';
